acda6f7ca9411df09007fc8d0f59f94202152b17
Gilbert Maerina <gilbert.maerina@gmail.com>
2015-12-13 09:07:32 -1000

    Updated script files and png outputs. Fixed issue with casting data to nuemrics from text

--- plot2.R
+++ plot2.R
@@ -11,7 +11,7 @@ endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
 png(filename='plot2.png',width=480,height=480,units='px')
 
 # plot data
-plot(df$Time,as.numeric(df$Global_active_power),xlim=c(startDay, endDay),ylab='Global Active Power (kilowatts)', xlab='', type="l")
+plot(df$Time,as.numeric(as.character(df$Global_active_power)),xlim=c(startDay, endDay),ylab='Global Active Power (kilowatts)', xlab='', type="l")
 
 axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
 
--- plot3.R
+++ plot3.R
@@ -8,12 +8,16 @@ png(filename='plot3.png',width=480,height=480,units='px')
 
 # plot data
 linecol<-c('black','red','blue')
-labls<-c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
-plot(df$Time,as.numeric(df$Sub_metering_1),type='l',col=linecol[1],xlab='',ylab='Energy sub metering')
-lines(df$Time,as.numeric(df$Sub_metering_2),col=linecol[2])
-lines(df$Time,as.numeric(df$Sub_metering_3),col=linecol[3])
 
-# add legend
+labls<-c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
+
+plot(df$Time,as.numeric(as.character(df$Sub_metering_1)),type='l',col=linecol[1],xlab='',ylab='Energy sub metering')
+
+lines(df$Time,as.numeric(as.character(df$Sub_metering_2)),col=linecol[2])
+
+lines(df$Time,as.numeric(as.character(df$Sub_metering_3)),col=linecol[3])
+
+# legend
 legend('topright',legend=labls,col=linecol,lty='solid')
 
 # close device
